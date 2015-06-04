76713c49888a42863995666dfd63fb92facc6d0b
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-06-04 16:01:08 +0200

    Accept option for animate document.title :dancers:

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  animateTitle: true,
+
   title: function(tokens) {
-    var base = 'My Blog';
+    var base = 'My Awesome Blog';
     var hasTokens = tokens && tokens.length;
 
     return hasTokens ? tokens.reverse().join(' - ') + ' - ' + base : base;

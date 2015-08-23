28376591f10cd9719d3667631fd132af223a12fb
Kim Røen <kim@kimroen.com>
2015-08-24 00:59:08 +0200

    Fix deprecated _actions call
    Use `_actions` pre-2.0, use `actions` after that.

--- vendor/document-title/document-title.js
+++ vendor/document-title/document-title.js
@@ -1,9 +1,6 @@
 var get = Ember.get;
 
-// Extend Ember.Route to add support for sensible
-// document.title integration.
-Ember.Route.reopen({
-
+var routeProps = {
   // `titleToken` can either be a static string or a function
   // that accepts a model object and returns a string (or array
   // of strings if there are multiple tokens).
@@ -14,44 +11,65 @@ Ember.Route.reopen({
   // that will be the document title. The `collectTitleTokens` action
   // stops bubbling once a route is encountered that has a `title`
   // defined.
-  title: null,
+  title: null
+};
 
-  // Provided by Ember
-  _actions: {
-    collectTitleTokens: function(tokens) {
-      var titleToken = get(this, 'titleToken');
-      if (typeof titleToken === 'function') {
-        titleToken = titleToken.call(this, get(this, 'currentModel'));
-      }
+/*
+  This is here because `_actions` was renamed to `actions` in Ember 2.0.0, but
+  we need it to work for versions before that.
 
-      if (Ember.isArray(titleToken)) {
-        tokens.unshift.apply(this, titleToken);
-      } else if (titleToken) {
-        tokens.unshift(titleToken);
-      }
+  Here, we inspect the `Ember.Route` prototype, iterate over its
+  `mergedProperties` to see what is used, and then use that.
+ */
+var mergedActionPropertyName = (function() {
+  var routeProto = Ember.Route.proto();
+  var mergedProps = routeProto.mergedProperties;
 
-      // If `title` exists, it signals the end of the
-      // token-collection, and the title is decided right here.
-      var title = get(this, 'title');
-      if (title) {
-        var finalTitle;
-        if (typeof title === 'function') {
-          finalTitle = title.call(this, tokens);
-        } else {
-          // Tokens aren't even considered... a string
-          // title just sledgehammer overwrites any children tokens.
-          finalTitle = title;
-        }
+  for (var i = 0, l = mergedProps.length; i < l; i++) {
+    var property = mergedProps[i];
 
-        // Stubbable fn that sets document.title
-        this.router.setTitle(finalTitle);
-      } else {
-        // Continue bubbling.
-        return true;
-      }
+    if (property === 'actions' || property === '_actions') {
+      return property;
     }
   }
-});
+})();
+
+routeProps[mergedActionPropertyName] = {
+  collectTitleTokens: function(tokens) {
+    var titleToken = get(this, 'titleToken');
+    if (typeof titleToken === 'function') {
+      titleToken = titleToken.call(this, get(this, 'currentModel'));
+    }
+
+    if (Ember.isArray(titleToken)) {
+      tokens.unshift.apply(this, titleToken);
+    } else if (titleToken) {
+      tokens.unshift(titleToken);
+    }
+
+    // If `title` exists, it signals the end of the
+    // token-collection, and the title is decided right here.
+    var title = get(this, 'title');
+    if (title) {
+      var finalTitle;
+      if (typeof title === 'function') {
+        finalTitle = title.call(this, tokens);
+      } else {
+        // Tokens aren't even considered... a string
+        // title just sledgehammer overwrites any children tokens.
+        finalTitle = title;
+      }
+
+      // Stubbable fn that sets document.title
+      this.router.setTitle(finalTitle);
+    } else {
+      // Continue bubbling.
+      return true;
+    }
+  }
+};
+
+Ember.Route.reopen(routeProps);
 
 Ember.Router.reopen({
   updateTitle: Ember.on('didTransition', function() {

800c49f45e17517dd6c755400231719949c552ec
Michael North <michael.l.north@gmail.com>
2017-12-15 21:12:26 -0600

    Add type information

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+declare module 'ember' {
+  /**
+   * NOTE: because of the way @types/ember works,
+   * merging a few properties into the Ember.Route interface
+   * will also result in the @ember/routing/route default export
+   * having these properties too.
+   * -- @mike-north
+   */
+  namespace Ember {
+    interface Route {
+      titleToken: string;
+      title: string;
+      setTitle: (title: string) => void;
+    }
+  }
+}

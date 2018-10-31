b9976bc21e7c32164fdcb018998e4447745f90a6
Jason Mitchell <jason.mitchell.w@gmail.com>
2018-10-31 14:28:16 -0700

    fix: Call super on `included` and use findHost API to find parent app

--- index.js
+++ index.js
@@ -3,12 +3,10 @@
 module.exports = {
   name: 'ember-cli-document-title',
 
-  included: function(app) {
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app;
-    }
+  included() {
+    this._super.included.apply(this, arguments);
 
+    const app = this._findHost();
     app.import('vendor/document-title/document-title.js');
   }
 };

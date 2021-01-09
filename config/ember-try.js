40eba88bb06da617fa5525f18f565d8bb1d9a589
Scott Newcomer <snewcomer@users.noreply.github.com>
2021-01-09 11:18:32 -0600

    CI: github actions (#99)
    * CI: github actions
    
    * add lint all
    
    * add dep
    
    * update test scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,30 +12,34 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.16',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true,
-            }),
-          },
+          name: 'ember-lts-3.8',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.16.0',
+              'ember-source': '~3.8.0',
             },
           },
         },
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true,
-            }),
-          },
+          name: 'ember-lts-3.12',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0',
+              'ember-source': '~3.12.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.16',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.16.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.20',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.20.0',
             },
           },
         },
